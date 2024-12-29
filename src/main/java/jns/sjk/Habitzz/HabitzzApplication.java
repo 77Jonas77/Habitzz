9ebe2be3009495fb1d8bf0c1233dc5ba7bc41c0d
s27523 <s27523@pjwstk.edu.pl>
2024-12-29 15:52:41 +0100

    Initial commit

---
+++ src/main/java/jns/sjk/Habitzz/HabitzzApplication.java
@@ -0,0 +1,13 @@
+package jns.sjk.Habitzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jns/sjk/Habitzz/HabitzzApplicationTests.java
@@ -0,0 +1,13 @@
+package jns.sjk.Habitzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
